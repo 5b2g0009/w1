cd833b62fb2efb41ea81cd7f1144cf5e9690536b
5b2g0009 <5b2g0009@stust.edu.tw>
2025-09-10 13:12:44 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 9;
+            for (int i=1;i<=n;j++)
+            {
+                for(int j=1;j<=n;j++)
+                {
+                    const.Write($"{i}*{j}={i * j");
+                }
+                 Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
